d3594b3609882da1188f0ea4c509a1e2a98115fd
Rob <rob@Robs-MacBook-Pro.local>
2026-03-14 17:40:20 -0500

    feat: exit menu dialog for pausing and leaving games

---
+++ src/components/exit-menu-dialog.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+interface ExitMenuDialogProps {
+  open: boolean;
+  onClose: () => void;
+  gameId: number;
+}
+
+export function ExitMenuDialog({ open, onClose, gameId }: ExitMenuDialogProps) {
+  return (
+    <Dialog open={open} onOpenChange={(v) => { if (!v) onClose(); }}>
+      <DialogContent className="max-w-xs">
+        <DialogHeader>
+          <DialogTitle>Game Menu</DialogTitle>
+        </DialogHeader>
+        <div className="flex flex-col gap-3 mt-2">
+          <Button onClick={onClose} size="lg" className="w-full">
+            Resume Game
+          </Button>
+          <a href="/">
+            <Button variant="outline" size="lg" className="w-full">
+              Save & Exit
+            </Button>
+          </a>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
